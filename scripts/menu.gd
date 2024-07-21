7e6a1dc0c8b5b176c6d5aa9c60cfd91b5b17f1de
viniciusmioto <vsmioto@gmail.com>
2024-07-21 00:29:49 -0400

    add: menu screen

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -3,7 +3,7 @@ extends Control
 func _ready():
 	# Connect the "pressed" signal of the Play and Quit buttons to their respective handlers
 	$MarginContainer/VBoxContainer/Play.connect("pressed", Callable(self, "_on_play_pressed"))
-	$MarginContainer/VBoxContainer/Play.connect("pressed", Callable(self, "_on_quit_pressed"))
+	$MarginContainer/VBoxContainer/Quit.connect("pressed", Callable(self, "_on_quit_pressed"))
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/playground.tscn")
